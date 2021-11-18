ada1cce8674f228e3b08e0453eca040e776222f9
Cory <oneillco117@gmail.com>
2021-11-18 09:36:51 -0500

    backend setup finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,8 @@ dingo = Dog.create(name: "Dingo", breed: "Australian Cattle Dog")
 riley = Dog.create(name: "Riley", breed: "Akita")
 jude = Dog.create(name: "Jude", breed: "Labrodor")
 spike = Dog.create(name: "Spike", breed: "Pitbull")
-snopp = Dog.create(name: "Snoop", breed: "Frenchy")
-frank = Dog.create(name: "Snoop", breed: "Pug")
+snoop = Dog.create(name: "Snoop", breed: "Frenchy")
+frank = Dog.create(name: "Frank", breed: "Pug")
 
 
 
@@ -15,5 +15,7 @@ swim = Deed.create(action: "Swim", category: "Exercise", dog_id: dingo.id)
 bed_time = Deed.create(action: "Sleep", category: "Relaxation", dog_id: jude.id)
 walk = Deed.create(action: "Walk", category: "Exercise", dog_id: riley.id)
 lunch = Deed.create(action: "Lunch", category: "Eating", dog_id: spike.id)
+frank = Deed.create(action: "Hike", category: "Exercise", dog_id: frank.id)
+snoop = Deed.create(action: "Treats", category: "Eating", dog_id: snoop.id)
 
 puts "✅ Done seeding!"
