8f98247994f1656a507e71563d76cacffd9619d1
Cory <oneillco117@gmail.com>
2021-11-20 20:22:51 -0500

    deleted unnecessary code

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,20 +2,6 @@ class Dog < ActiveRecord::Base
   has_many :deeds, dependent: :destroy 
 
   validates :name, presence: true
-
-  def dog_count
-    self.dogs.count
-  end
-
-  def deed_count
-    self.deeds.count
-  end
-
-  def all_deeds_names
-   self.deeds.collect do |t|
-    t.name
-   end
-
 end
 
 
